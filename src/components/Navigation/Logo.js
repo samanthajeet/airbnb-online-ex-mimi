8f185f5d826262a0110910d7da6e84ff6126eb7a
Mimi <mimitraugott@gmail.com>
2020-06-08 19:02:48 -0600

    main page wip

--- src/components/Navigation/Logo.js
+++ src/components/Navigation/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./Logo.css"
 
 const Logo = () => {
     return (
         <div className="">
-            <h1>Logo</h1>
+            <img className="airbnb-logo" src="https://www.dirs21.de/wp-content/uploads/2018/11/airbnb_horizontal_lockup_white_web-high-res.png" alt="Airbnb logo"/>
         </div>
     )
 }
